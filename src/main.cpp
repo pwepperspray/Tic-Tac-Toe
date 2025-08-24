6947bf9e3ee9995a75ad4624946e16764d2d479e
cinnamon_rowll <166984309+pwepperspray@users.noreply.github.com>
2025-08-24 16:15:47 +0530

    Revamped everything It should be working well now

--- src/main.cpp
+++ src/main.cpp
@@ -1,97 +1,184 @@
+/*
+A simple console Tic-Tac-Toe game in C.
+Author : cinnamon_rowll
+Github : @pwepperspray
+*/
+
+//NOTE : In user defined functions of int return value, 0 is false and 1 is true.
+
 #include <stdio.h>
+#include <conio.h>
+
+void set_up_grid();
+void display();
+void clear_screen();
+void run_game(char p1, char p2);
+int insert(int x, int y, char val);
+int check_win(char player_symbol);
+int is_grid_full();
+
+char grid[3][3];
 
 int main(){
-	int x,y,state,count = 1, run = 0;
-	char arr[3][3] = {{' ',' ',' '},
-		          {' ',' ',' '},
-			  {' ',' ',' '}};
+    char p1, p2, choice;
+    //-------------menu-------------//
+    while(1){
+        clear_screen();
+        printf("Tic Tac Toe game! \nInput C to exit. \n");
+        printf("Select which will start first : X or O \n>>");
+        scanf(" %c", &choice);
 
-	printf("Tic-Tac-Toe Game\n\n");
-	for(int i = 0; i < 3; i++){
-		printf("%3c%3c%3c%3c%3c\n", arr[i][0], '|', arr[i][1], '|', arr[i][2]);
-		if(i < 2){
-			printf("-----------------\n");
-		} 
-	}
-
-	while(count < 10){	
-		//input block
-		printf("\nPlayer X enter coordinates : ");
-		scanf("%d%d", &x, &y);
-		if(arr[x - 1][y - 1] == ' '){
-		arr[x - 1][y - 1] = 'X';
-			count++;
-		}
-		else{
-			printf("Enter Coordinate of empty spaces\n");
-		}	
-
-		for(int i = 0; i < 3; i++){
-			printf("%3c%3c%3c%3c%3c\n", arr[i][0], '|', arr[i][1], '|', arr[i][2]);
-			if(i < 2){
-				printf("-----------------\n");
-			} 
-		}
-
-		printf("\nPlayer O enter coordinates : ");
-		scanf("%d%d", &x, &y);
-		if(arr[x - 1][y - 1] == ' '){
-			arr[x - 1][y - 1] = 'O';
-			count++;
-		}
-		else{
-			printf("Enter Coordinate of empty spaces\n");
-		}	
-
-		for(int i = 0; i < 3; i++){
-			printf("%3c%3c%3c%3c%3c\n", arr[i][0], '|', arr[i][1], '|', arr[i][2]);
-			if(i < 2){
-				printf("-----------------\n");
-			} 
-		}
-
-		/*************************************check block*********************************/
-		//if rows match
-		for(int i = 0; i < 3; i++){
-			if(arr[i][0] != ' ' && arr[i][1] != ' ' && arr[i][2] != ' '){
-				if(arr[i][0] == arr[i][1] && arr[i][1] == arr[i][2]){
-					printf("%c Won!\n", arr[i][0]);
-					count = 11;
-				}
-			}
-		}
-
-		//if columns match
-		for(int i = 0; i < 3; i++){
-			if(arr[0][i] != ' ' && arr[1][i] != ' ' && arr[2][i] != ' '){
-				if(arr[0][i] == arr[1][i] && arr[1][i] == arr[2][i]){
-					printf("%c Won!\n", arr[0][i]);
-					count = 11;
-				}
-			}
-		}
-
-		//if first diagonal matches
-		if(arr[0][0] != ' ' && arr[1][1] != ' ' && arr[2][2] != ' '){
-			if(arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2]){
-				printf("%c Won!\n", arr[1][1]);
-				break;
-			}
-		}
-
-		//if second diagonal matches
-		if(arr[0][2] != ' ' && arr[1][1] != ' ' && arr[2][0] != ' '){
-			if(arr[0][2] == arr[1][1] && arr[1][1] == arr[2][0]){
-				printf("%c Won!\n", arr[1][1]);
-				break;
-			}
-		}
-	}
-	if(count == 9){
-		printf("No one won \n");
-	}
-
-	return 0;
+        if(choice == 'x' || choice == 'X'){
+            p1 = 'X';
+            p2 = 'O';
+            run_game(p1,p2);
+        }else if(choice == 'o' || choice == 'O'){
+            p1 = 'O';
+            p2 = 'X';
+            run_game(p1,p2);
+        }else if(choice == 'c' || choice == 'C'){
+            printf("Thanks For Playing!");
+            break;
+        }else{
+            printf("Enter a valid value");
+        }
+    }
+    return 0;
 }
 
+
+void set_up_grid(){
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j <3; j++){
+            grid[i][j] = ' ';
+        }
+    }
+}
+
+void display(){
+    for(int i = 0; i < 3; i++){
+        printf("%3c%3c%3c%3c%3c\n", grid[i][0], '|', grid[i][1], '|', grid[i][2]);
+        if(i < 2){
+            printf("-----------------\n");
+        } 
+    }
+}
+
+void clear_screen(){
+    printf("\e[1;1H\e[2J"); 
+}
+	
+void run_game(char p1, char p2){
+    int x,y;
+
+    set_up_grid();
+    clear_screen();
+
+    while(1){
+        //Player 1 turn
+        while(1){
+            display();
+            printf("Player %c enter coordinate : ", p1);
+            scanf("%d %d", &x,&y);
+            if(x <= 3 && x > 0 && y <= 3 && y > 0){
+                if(insert(x,y,p1)){
+                    break;
+                }
+            }else{
+                printf("Enter a valid value\n");
+            }     
+        }
+
+        if(check_win(p1)){
+            printf("Player %c has Won!", p1);
+            getch();
+            break;
+        }
+
+        if(is_grid_full()){
+            printf("Grid is full! NOBODY WINS!");
+            getch();
+            break;
+        }
+
+        clear_screen();
+
+        //Player 2 turn    
+        while(1){
+            display();
+            printf("Player %c enter coordinate : ", p2);
+            scanf("%d %d", &x,&y);
+            if(x <= 3 && x > 0 && y <= 3 && y > 0){
+                if(insert(x,y,p2)){
+                    break;
+                }
+            }else{
+                printf("Enter a valid value\n");
+            }
+        }
+
+        if(check_win(p2)){
+            printf("Player %c has Won!", p1);
+            getch();
+            break;
+        }
+
+        if(is_grid_full()){
+            printf("Grid is full! NOBODY WINS!");
+            getch();
+            break;
+        }
+
+        clear_screen();
+    }
+}
+
+int insert(int x, int y, char val){
+	if(grid[x - 1][y -1] == ' '){
+        grid[x - 1][y - 1] = val;
+        return 1;
+    }else{
+        printf("Enter an Empty cell coordinate \n");
+    }
+    return 0;
+}
+
+int check_win(char player_symbol){
+   //Row check
+    for (int i = 0; i < 3; i++) {
+        if (grid[i][0] == player_symbol && grid[i][1] == player_symbol && grid[i][2] == player_symbol) {
+            return 1; // Win
+        }
+    }
+    //Column Check
+    for (int j = 0; j < 3; j++) {
+        if (grid[0][j] == player_symbol && grid[1][j] == player_symbol && grid[2][j] == player_symbol) {
+            return 1; // Win
+        }
+    }
+    //diagonal Check
+    if ((grid[0][0] == player_symbol && grid[1][1] == player_symbol && grid[2][2] == player_symbol) ||
+        (grid[0][2] == player_symbol && grid[1][1] == player_symbol && grid[2][0] == player_symbol)) {
+        return 1; // Win
+    }
+
+    return 0;
+}
+
+int is_grid_full(){
+    int count = 0;
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            if(grid[i][j] != ' '){
+                count++;
+            }
+        }
+    }
+
+    if(count < 9){
+        return 1;
+    }
+
+    return 0;
+}
 
